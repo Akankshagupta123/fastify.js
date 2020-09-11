d5adf14e563b51c7635fe162c652b9c2a637a083
Akankshagupta123 <49816094+Akankshagupta123@users.noreply.github.com>
2020-09-11 13:31:14 +0530

    Code file uploaded successfully !!

---
+++ fastify.js
@@ -0,0 +1,8 @@
+// server.js
+'use strict'
+
+module.exports = async function (fastify, opts) {
+  fastify.get('/g', async (request, reply) => {
+    reply.redirect('https://www.google.com')
+  })
+}
